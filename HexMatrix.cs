d179505d7d9f75302ce7fef110bf1a3fd823fe6e
Thiago <ThiagoDAraujoS@Gmail.com>
2019-11-26 10:47:30 -0800

    Separated Matrix and Vector classes into their own files

---
+++ HexMatrix.cs
@@ -0,0 +1,40 @@
+﻿using System;
+public class HexMatrix<T>
+{
+    private T[] data;
+
+    private int length;
+
+    private int MapHexInto1DArray(int x, int y) {
+        return length * y + x + y / 2;
+    }
+
+    private VectorHex Project1DArrayIntoHex(int i) {
+        int y = i / length;
+        int x = i % length - y / 2;
+        return new VectorHex(x, y,-x -y);
+    }
+
+    public T this[int x, int y, int z = 0] {
+        get {   return data[MapHexInto1DArray(x, y)];}
+        set {   data[MapHexInto1DArray(x, y)] = value;}
+    }
+
+    public T this[VectorHex i] {
+        get{    return this[i.x, i.y]; }
+        set{    this[i.x, i.y] = value; }
+    }
+
+    public HexMatrix(int length, int height) {
+        this.length = length;
+        data = new T[length * height];
+    }
+
+    public delegate void ForeachOperation(VectorHex index, ref T item);
+    
+    public void Foreach (ForeachOperation action) {
+        for (int i = 0; i < data.Length; i++)
+            action(Project1DArrayIntoHex(i), ref data[i]);
+    }
+    
+}
--- VectorHex.cs
+++ VectorHex.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 ///<summary>Vector Hex is a int vector3 like struct that contain hexagon coordinates, it also provides methods to deal with those coordinates and some shorthand readonly values</summary>
 public struct VectorHex{
@@ -68,43 +68,4 @@ public struct VectorHex{
             num.z / scalar
         );
     }
-}
-public class HexMatrix<T>
-{
-    private T[] data;
-
-    private int length;
-
-    private int MapHexInto1DArray(int x, int y) {
-        return length * y + x + y / 2;
-    }
-
-    private VectorHex Project1DArrayIntoHex(int i) {
-        int y = i / length;
-        int x = i % length - y / 2;
-        return new VectorHex(x, y,-x -y);
-    }
-
-    public T this[int x, int y, int z = 0] {
-        get {   return data[MapHexInto1DArray(x, y)];}
-        set {   data[MapHexInto1DArray(x, y)] = value;}
-    }
-
-    public T this[VectorHex i] {
-        get{    return this[i.x, i.y]; }
-        set{    this[i.x, i.y] = value; }
-    }
-
-    public HexMatrix(int length, int height) {
-        this.length = length;
-        data = new T[length * height];
-    }
-
-    public delegate void ForeachOperation(VectorHex index, ref T item);
-    
-    public void Foreach (ForeachOperation action) {
-        for (int i = 0; i < data.Length; i++)
-            action(Project1DArrayIntoHex(i), ref data[i]);
-    }
-    
 }
