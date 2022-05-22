e3771da447ee119827badd3efde5926f05df7ab7
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2022-05-21 18:02:10 -0700

    Made vector variables readonly

--- Vector.cs
+++ Vector.cs
@@ -7,7 +7,7 @@ namespace Hex {
     /// </summary>
     public struct Vector {
         //x, y, z values
-        public int x, y, z;
+        public readonly int x, y, z;
 
         ///<summary>
         /// Builds vector from xyz coordinates
@@ -21,7 +21,7 @@ namespace Hex {
         public Vector(int x, int y) {
             this.x = x;
             this.y = y;
-            this.z = -x - y;
+            z = -x - y;
         }
 
         public static readonly Vector XPos = new Vector(0, 1, -1);
@@ -33,14 +33,14 @@ namespace Hex {
         public static readonly Vector ZPos = new Vector(1, -1, 0);
         public static readonly Vector ZNeg = new Vector(-1, 1, 0);
 
-        public static readonly float SqrOf3 = Mathf.Sqrt(3f);
+        public static readonly float RootOf3 = Mathf.Sqrt(3f);
 
         ///<summary>
         /// Returns a physical position of a given hexagon inside this container
         /// </summary>
-        public Vector3 Position(float size, float height = 0f) =>
+        public Vector3 Position(float size = 1f, float height = 0f) =>
             new Vector3(
-                size * (SqrOf3 * (float) x + SqrOf3 / 2f * (float) y),
+                size * (RootOf3 * x + RootOf3 / 2f * y),
                 height,
                 size * (3f / 2f * y));
 
@@ -49,7 +49,7 @@ namespace Hex {
 
         public bool IsLegal => x + y + z == 0;
 
-        public static implicit operator Vector3(Vector v) => v.Position(1f);
+        public static implicit operator Vector3(Vector v) => v.Position();
 
         public void Validate() {
 #if(UNITY_EDITOR)
@@ -72,23 +72,19 @@ namespace Hex {
         /// Rounds a float Hex vector into a int hex vector
         /// </summary>
         public static Vector Round(Vector3 vector) {
-            Vector rounded = new Vector(
-                Mathf.RoundToInt(vector.x),
-                Mathf.RoundToInt(vector.y),
-                Mathf.RoundToInt(vector.z)
-            );
-            Vector3 diff = new Vector3(
-                Mathf.Abs(rounded.x - vector.x),
-                Mathf.Abs(rounded.y - vector.y),
-                Mathf.Abs(rounded.z - vector.z)
-            );
-            if (diff.x > diff.y && diff.x > diff.z)
-                rounded.x = -rounded.y - rounded.z;
-            else if (diff.y > diff.z)
-                rounded.y = -rounded.x - rounded.z;
+            int roundX = Mathf.RoundToInt(vector.x),
+                roundY = Mathf.RoundToInt(vector.y),
+                roundZ = Mathf.RoundToInt(vector.z);
+            float diffX = Mathf.Abs(roundX - vector.x),
+                  diffY = Mathf.Abs(roundY - vector.x),
+                  diffZ = Mathf.Abs(roundZ - vector.x);
+            if (diffX > diffY && diffX > diffZ)
+                roundX = -roundY - roundZ;
+            else if (diffY > diffZ)
+                roundY = -roundX - roundZ;
             else
-                rounded.z = -rounded.x - rounded.y;
-            return rounded;
+                roundZ = -roundX - roundY;
+            return new Vector(roundX, roundY, roundZ);
         }
     }
 
@@ -97,7 +93,7 @@ namespace Hex {
         /// Returns a hexagon ID from a x and z coordinate position
         ///</summary>
         public static Vector PointToHexVector(this Vector3 point, float size = 1f) {
-            float ix = (Vector.SqrOf3 / 3f * point.x - 1f / 3f * point.z) / size;
+            float ix = (Vector.RootOf3 / 3f * point.x - 1f / 3f * point.z) / size;
             float iy = (2f / 3f * point.z) / size;
             return Vector.Round(new Vector3(ix, iy, -ix - iy));
         }
